0f62941a0a4e09f2973bb2eddb544df0bc709c65
Phạm Nguyễn Xuân Trường <76194393+pnxuantruong@users.noreply.github.com>
2022-07-07 09:33:41 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+ import React, { useState} from 'react';
+ import AppNavigator from './components/appNavigator';
+ 
+ export default App = () => {
+   return (
+     <AppNavigator/>
+   )
+ }
+ 
